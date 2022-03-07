f0e93350a462759e207f52112f50c39a0bd516ab
DanangFirmantara <danangfirmantara837@gmail.com>
2022-03-07 09:45:04 +0700

    fix : update data postman and mysql

--- vehicle_rent.sql
+++ vehicle_rent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 25 Feb 2022 pada 14.47
+-- Waktu pembuatan: 06 Mar 2022 pada 20.12
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `forget_request`
+-- Struktur dari tabel `forgot_request`
 --
 
-CREATE TABLE `forget_request` (
+CREATE TABLE `forgot_request` (
   `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `code` int(11) NOT NULL,
@@ -37,10 +37,10 @@ CREATE TABLE `forget_request` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `forget_request`
+-- Dumping data untuk tabel `forgot_request`
 --
 
-INSERT INTO `forget_request` (`id`, `userId`, `code`, `isExpired`, `createdAt`, `updatedAt`) VALUES
+INSERT INTO `forgot_request` (`id`, `userId`, `code`, `isExpired`, `createdAt`, `updatedAt`) VALUES
 (2, 85, 194454, 1, '2022-02-13 21:25:15', '2022-02-13 21:28:01'),
 (3, 85, 304006, 1, '2022-02-13 21:25:40', '2022-02-13 21:28:28'),
 (4, 85, 496246, 1, '2022-02-13 21:25:57', '2022-02-13 21:28:28'),
@@ -57,11 +57,7 @@ INSERT INTO `forget_request` (`id`, `userId`, `code`, `isExpired`, `createdAt`, 
 (15, 85, 237485, 1, '2022-02-13 22:49:03', '2022-02-13 22:50:15'),
 (16, 85, 498371, 1, '2022-02-13 22:50:24', '2022-02-13 22:50:33'),
 (17, 85, 457860, 1, '2022-02-14 00:01:47', '2022-02-14 00:06:48'),
-(24, 90, 820483, 1, '2022-02-14 16:04:50', '2022-02-14 16:18:49'),
-(25, 90, 697715, 1, '2022-02-14 23:12:50', '2022-02-14 23:16:22'),
-(26, 90, 920678, 1, '2022-02-14 23:16:29', '2022-02-14 23:17:31'),
-(27, 90, 260663, 1, '2022-02-14 23:18:44', '2022-02-14 23:21:17'),
-(28, 90, 254522, 1, '2022-02-14 23:21:21', '2022-02-14 23:21:57');
+(41, 91, 234841, 1, '2022-03-01 19:49:59', '2022-03-01 20:41:06');
 
 -- --------------------------------------------------------
 
@@ -87,17 +83,22 @@ CREATE TABLE `histories` (
 
 INSERT INTO `histories` (`id`, `rentStartDate`, `rentEndDate`, `prepayment`, `userId`, `vehicleId`, `quantity`, `createdAt`, `updatedAt`) VALUES
 (3, '2022-02-02', '2022-02-04', 1, NULL, 30, 0, '2022-02-02 15:54:31', '2022-02-02 23:27:38'),
-(11, '2022-02-02', '2022-02-04', 1, NULL, 29, 0, '2022-02-02 17:38:20', '2022-02-02 23:27:38'),
 (12, '2022-02-02', '2022-02-04', 1, NULL, 30, 0, '2022-02-02 17:39:18', '2022-02-02 23:27:38'),
 (13, '2022-02-02', '2022-02-04', 1, NULL, 30, 0, '2022-02-02 17:53:21', '2022-02-02 23:27:38'),
-(16, '2022-01-02', '2022-01-03', 1, NULL, 30, 0, '2022-02-02 22:15:56', '2022-02-02 23:27:38'),
-(17, '2022-01-02', '2022-01-03', 1, NULL, 30, 0, '2022-02-02 22:21:39', '2022-02-02 23:27:38'),
 (21, '2022-01-02', '2022-01-03', 1, NULL, 30, 0, '2022-02-02 22:36:07', '2022-02-02 23:27:38'),
 (23, '2022-01-07', '2022-01-08', 1, NULL, 30, 0, '2022-02-06 13:45:56', '2022-02-06 14:52:58'),
 (24, '2022-01-07', '2022-01-08', 1, NULL, 47, 0, '2022-02-07 09:53:29', '2022-02-08 22:48:42'),
 (25, '2022-01-07', '2022-01-08', 1, NULL, 47, 0, '2022-02-07 09:54:30', '2022-02-08 22:46:49'),
 (26, '2022-01-07', '2022-01-08', 1, NULL, 29, 0, '2022-02-08 22:33:49', '2022-02-08 22:42:57'),
-(27, '2022-01-07', '2022-01-08', 1, NULL, 29, 0, '2022-02-08 22:51:05', '2022-02-08 22:51:14');
+(27, '2022-01-07', '2022-01-08', 1, NULL, 29, 0, '2022-02-08 22:51:05', '2022-02-08 22:51:14'),
+(35, '2022-03-06', '2022-03-08', 0, 91, 45, 1, '2022-03-06 20:44:10', NULL),
+(36, '2022-03-07', '2022-03-08', 0, 91, 69, 1, '2022-03-06 20:44:40', NULL),
+(37, '2022-03-02', '2022-03-04', 0, 96, 30, 1, '2022-03-06 20:44:58', NULL),
+(38, '2022-03-02', '2022-03-04', 0, 96, 30, 0, '2022-03-06 21:05:59', '2022-03-06 21:45:31'),
+(43, '2022-03-02', '2022-03-04', 0, 96, 30, 0, '2022-03-06 21:18:48', '2022-03-06 21:44:23'),
+(44, '2022-03-02', '2022-03-04', 0, 96, 30, 3, '2022-03-06 23:04:20', NULL),
+(45, '2022-03-08', '2022-03-09', 0, 91, 29, 1, '2022-03-07 00:09:42', NULL),
+(46, '2022-03-07', '2022-03-08', 0, 96, 31, 1, '2022-03-07 00:24:17', NULL);
 
 --
 -- Trigger `histories`
@@ -150,7 +151,7 @@ DELIMITER ;
 
 CREATE TABLE `prepayment` (
   `id` int(11) NOT NULL,
-  `status` varchar(80) DEFAULT NULL
+  `status` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -158,8 +159,8 @@ CREATE TABLE `prepayment` (
 --
 
 INSERT INTO `prepayment` (`id`, `status`) VALUES
-(1, 'payment completed'),
-(2, 'payment not completed');
+(1, 'Payment'),
+(2, 'invalid payment');
 
 -- --------------------------------------------------------
 
@@ -178,6 +179,7 @@ CREATE TABLE `users` (
   `address` varchar(255) DEFAULT NULL,
   `displayName` varchar(255) DEFAULT NULL,
   `birthDate` date DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
   `createdAt` datetime DEFAULT current_timestamp(),
   `updatedAt` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -186,11 +188,15 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `contact`, `password`, `fullName`, `gender`, `address`, `displayName`, `birthDate`, `createdAt`, `updatedAt`) VALUES
-(85, 'hamdi', 'hamdi@gmail.com', '082398767584', '$2b$10$1a87N11JZJx.C73VidOfNOjqXkFZMo3Lbwlqhc1Ksja3rUQCqLZja', NULL, NULL, NULL, NULL, NULL, '2022-02-13 01:28:26', '2022-02-14 00:06:48'),
-(86, 'asgigih', 'gigih@gmail.com', '087627361711', '$2b$10$jUW5O1vum8NfHIjR5sUp4OB8OcxjMzTNHuQdnYRW9J8HcwC2AgsmK', NULL, NULL, NULL, NULL, NULL, '2022-02-13 01:29:19', NULL),
-(88, 'danang', 'danang@gmail.com', '087898439385', '$2b$10$FgsIgBOQPOeVTHyVISYrzONjX2cjlrBeMSrdal2wKN9fAEksbs0c6', 'Danang firmantara', 'Male', 'Padang', 'Danang', '1999-05-09', '2022-02-13 01:32:21', '2022-02-13 01:35:12'),
-(90, 'admin', 'gimivig886@plexfirm.com', '082932832345', '$2b$10$DSb5yetOtLL9G4fNgoH6/eP6yfcbrI2QRAJZaXKE66zvnUAu8h7sm', NULL, NULL, NULL, NULL, NULL, '2022-02-14 16:04:43', '2022-02-14 23:21:57');
+INSERT INTO `users` (`id`, `username`, `email`, `contact`, `password`, `fullName`, `gender`, `address`, `displayName`, `birthDate`, `image`, `createdAt`, `updatedAt`) VALUES
+(85, 'hamdi', 'hamdi@gmail.com', '082398767584', '$2b$10$1a87N11JZJx.C73VidOfNOjqXkFZMo3Lbwlqhc1Ksja3rUQCqLZja', NULL, NULL, NULL, NULL, NULL, NULL, '2022-02-13 01:28:26', '2022-02-14 00:06:48'),
+(86, 'asgigih', 'gigih@gmail.com', '087627361711', '$2b$10$jUW5O1vum8NfHIjR5sUp4OB8OcxjMzTNHuQdnYRW9J8HcwC2AgsmK', NULL, NULL, NULL, NULL, NULL, NULL, '2022-02-13 01:29:19', NULL),
+(88, 'danang', 'danang@gmail.com', '087898439385', '$2b$10$FgsIgBOQPOeVTHyVISYrzONjX2cjlrBeMSrdal2wKN9fAEksbs0c6', 'Danang firmantara', 'Male', 'Padang', 'Danang', '1999-05-09', NULL, '2022-02-13 01:32:21', '2022-02-13 01:35:12'),
+(91, 'admin', 'bevad56184@ishop2k.com', '081237844675', '$2b$10$wCIQq83SEkqqmm6fNusboOhFp7PX.9JSqtn.dNJoGpIcDCBCVfxs2', NULL, 'Male', 'bandung', 'Admin', '1999-02-15', NULL, '2022-03-01 19:26:31', '2022-03-05 18:14:46'),
+(95, 'dinda', 'dinda@gmail.com', NULL, '$2b$10$Xn56FWUmDwTZqBP5nIJjveO/oRoNE.UkEn9hKOg5llCyWlQpQjAHO', NULL, NULL, NULL, NULL, NULL, NULL, '2022-03-05 11:57:42', NULL),
+(96, 'fajri', 'fajri@mail.com', '087898439385', '$2b$10$5G8U4okEmILCDAJUHEQ1q.MXuvNMOYZv6QadGp1XBPOURjZ7dGQdC', 'Fajri saja', 'Male', 'Padang', NULL, NULL, NULL, '2022-03-05 13:08:36', '2022-03-05 17:56:08'),
+(97, 'putri', 'putri@mail.com', NULL, '$2b$10$HggkoXlyvl4wJztX8A34KOgqiixndj0hpkgXeZo771D4t3jiKuAY2', NULL, NULL, NULL, NULL, NULL, NULL, '2022-03-05 13:25:36', NULL),
+(98, 'joni', 'joni@mail.com', NULL, '$2b$10$cSZ8MVqdmdTbBNDwymlSYenSh8TrNYbobRM4Ut7MmKI4FuaEkwHHy', NULL, NULL, NULL, NULL, NULL, NULL, '2022-03-05 13:31:49', NULL);
 
 -- --------------------------------------------------------
 
@@ -217,26 +223,26 @@ CREATE TABLE `vehicles` (
 --
 
 INSERT INTO `vehicles` (`id`, `category`, `name`, `location`, `description`, `price`, `status`, `stock`, `image`, `createdAt`, `updatedAt`) VALUES
-(21, 'Bike', 'Fixie', 'Padang', 'Up to 1 Person, return before 4 PM', 20000, 'Available', 4, NULL, '2022-02-01 21:15:39', '2022-02-25 15:49:06'),
-(29, 'Car', 'Toyota Vellfire', 'Yogyakarta', 'Up to 5 Person, return before 4 PM', 400000, 'Available', 5, 'uploads/image-24-1-2022-480753439.png', '2022-02-01 22:38:41', '2022-02-24 21:56:58'),
-(30, 'Car', 'Toyota Alphard', 'Yogyakarta', 'Up to 5 Person, return before 4 PM', 400000, 'Available', 5, 'uploads/image-24-1-2022-18442857.png', '2022-02-01 23:09:57', '2022-02-24 20:27:45'),
-(31, 'Car', 'Toyota Fortuner', 'Yogyakarta', 'Up to 5 adult, return before 4 PM', 400000, 'Available', 5, 'uploads/image-24-1-2022-656306862.png', '2022-02-03 20:12:32', '2022-02-24 20:28:35'),
-(32, 'Car', 'Toyota Fortuner', 'Jakarta', 'Up to 5 adult, return before 4 PM', 400000, 'Available', 5, 'uploads/image-24-1-2022-513878679.png', '2022-02-03 20:16:38', '2022-02-24 20:45:33'),
-(33, 'Car', 'Toyota Fortuner', 'Padang', 'Up to 5 adult, return before 4 PM', 400000, 'Available', 5, NULL, '2022-02-03 20:16:45', '2022-02-25 15:49:11'),
-(34, 'Car', 'Toyota Alphard', 'Jakarta', 'Up to 5 Person, return before 4 PM', 400000, 'Available', 5, NULL, '2022-02-03 20:18:03', '2022-02-25 15:49:14'),
+(21, 'Bike', 'Fixie', 'Padang', 'Up to 1 Person, return before 4 PM', 20000, 'Available', 4, 'uploads/image-4-2-2022-379929505.png', '2022-02-01 21:15:39', '2022-03-04 20:18:59'),
+(29, 'Car', 'Toyota Vellfire', 'Yogyakarta', 'Up to 5 Person, return before 4 PM', 400000, 'Available', 4, 'uploads/image-4-2-2022-334159266.png', '2022-02-01 22:38:41', '2022-03-07 00:09:42'),
+(30, 'Car', 'Toyota Alphard', 'Yogyakarta', 'Up to 5 Person, return before 4 PM', 400000, 'Available', 1, 'uploads/image-4-2-2022-839211924.png', '2022-02-01 23:09:57', '2022-03-06 23:04:20'),
+(31, 'Car', 'Toyota Fortuner', 'Yogyakarta', 'Up to 5 adult, return before 4 PM', 400000, 'Available', 4, 'uploads/image-4-2-2022-944097651.png', '2022-02-03 20:12:32', '2022-03-07 00:24:17'),
+(32, 'Car', 'Toyota Fortuner', 'Jakarta', 'Up to 5 adult, return before 4 PM', 400000, 'Available', 5, 'uploads/image-4-2-2022-887172732.png', '2022-02-03 20:16:38', '2022-03-04 20:10:05'),
+(33, 'Car', 'Toyota Fortuner', 'Padang', 'Up to 5 adult, return before 4 PM', 400000, 'Available', 5, 'uploads/image-4-2-2022-4548440.png', '2022-02-03 20:16:45', '2022-03-04 20:10:37'),
+(34, 'Car', 'Toyota Alphard', 'Jakarta', 'Up to 5 Person, return before 4 PM', 400000, 'Available', 5, 'uploads/image-4-2-2022-500666015.png', '2022-02-03 20:18:03', '2022-03-04 20:06:23'),
 (37, 'Car', 'Honda Brio', 'Yogyakarta', 'Up to 4 adult, return before 4 PM', 300000, 'Available', 5, 'uploads/image-24-1-2022-733470970.png', '2022-02-07 08:41:34', '2022-02-24 20:35:17'),
-(38, 'Car', 'Honda Jazz', 'Yogyakarta', 'Up to 4 adult, return before 4 PM', 350000, 'Available', 5, NULL, '2022-02-07 08:42:02', '2022-02-25 15:51:22'),
-(39, 'Car', 'Daihatsu xenia', 'Yogyakarta', 'Up to 4 adult, return before 4 PM', 300000, 'Available', 5, NULL, '2022-02-07 08:42:32', '2022-02-25 15:49:22'),
-(40, 'Bike', 'Fixie', 'Yogyakarta', 'Up to 1 adult, return before 4 PM', 50000, 'Available', 5, NULL, '2022-02-07 08:43:37', '2022-02-25 15:49:19'),
-(41, 'Bike', 'BMX', 'Yogyakarta', 'Up to 1 adult, return before 4 PM', 50000, 'Available', 5, NULL, '2022-02-07 08:47:55', '2022-02-25 15:51:43'),
-(42, 'Bike', 'Family bike', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 5, NULL, '2022-02-07 08:48:47', '2022-02-25 15:51:45'),
+(38, 'Car', 'Honda Jazz', 'Yogyakarta', 'Up to 4 adult, return before 4 PM', 350000, 'Available', 5, 'uploads/image-4-2-2022-680648005.png', '2022-02-07 08:42:02', '2022-03-04 20:13:21'),
+(39, 'Car', 'Daihatsu Xenia', 'Yogyakarta', 'Up to 4 adult, return before 4 PM', 300000, 'Available', 5, 'uploads/image-4-2-2022-371501686.png', '2022-02-07 08:42:32', '2022-03-04 20:16:07'),
+(40, 'Bike', 'Fixie', 'Yogyakarta', 'Up to 1 adult, return before 4 PM', 50000, 'Available', 5, 'uploads/image-4-2-2022-92552114.png', '2022-02-07 08:43:37', '2022-03-04 20:19:17'),
+(41, 'Bike', 'BMX', 'Yogyakarta', 'Up to 1 adult, return before 4 PM', 50000, 'Available', 5, 'uploads/image-4-2-2022-754005160.png', '2022-02-07 08:47:55', '2022-03-04 20:19:57'),
+(42, 'Bike', 'family bike', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 5, 'uploads/image-4-2-2022-999321599.png', '2022-02-07 08:48:47', '2022-03-04 20:20:27'),
 (44, 'Motorbike', 'Honda Beat Street', 'Yogyakarta', 'Up to 2 Person, return before 4 PM', 40000, 'Available', 4, 'uploads/image-14-1-2022-739687107.png', '2022-02-07 08:58:44', '2022-02-14 23:39:00'),
-(45, 'Motorbike', 'Honda Scoopy', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 4, NULL, '2022-02-07 08:59:42', '2022-02-25 15:51:28'),
-(46, 'Motorbike', 'Yamaha FreeGo', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 4, NULL, '2022-02-07 09:00:52', '2022-02-25 15:51:37'),
-(47, 'Motorbike', 'Honda Vario 125', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 7, NULL, '2022-02-07 09:01:31', '2022-02-25 15:51:40'),
-(48, 'Car', 'Honda Jazz', 'Jakarta', 'Up to 4 adult, return before 4 PM', 500000, 'Available', 4, NULL, '2022-02-08 20:24:11', '2022-02-25 15:51:25'),
+(45, 'Motorbike', 'Honda Scoopy', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 3, 'uploads/image-4-2-2022-868313119.png', '2022-02-07 08:59:42', '2022-03-06 20:44:10'),
+(46, 'Motorbike', 'Yamaha FreeGo', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 4, 'uploads/image-4-2-2022-67911215.png', '2022-02-07 09:00:52', '2022-03-04 20:18:09'),
+(47, 'Motorbike', 'Honda Vario 125', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 7, 'uploads/image-4-2-2022-960063133.png', '2022-02-07 09:01:31', '2022-03-04 20:07:51'),
+(48, 'Car', 'Honda Jazz', 'Jakarta', 'Up to 4 adult, return before 4 PM', 500000, 'Available', 4, 'uploads/image-4-2-2022-727264533.png', '2022-02-08 20:24:11', '2022-03-04 20:13:52'),
 (49, 'Car', 'Honda Brio', 'Jakarta', 'Up to 4 adult, return before 4 PM', 300000, 'Available', 4, NULL, '2022-02-08 21:19:06', '2022-02-25 15:51:31'),
-(69, 'Motorbike', 'Yamaha Nmax', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 4, 'uploads/image-14-1-2022-197474077.png', '2022-02-12 23:28:02', '2022-02-14 23:40:39'),
+(69, 'Motorbike', 'Yamaha Nmax', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 3, 'uploads/image-14-1-2022-197474077.png', '2022-02-12 23:28:02', '2022-03-06 20:44:40'),
 (71, 'Motorbike', 'Yamaha Vixion R', 'Yogyakarta', 'Up to 2 adult, return before 4 PM', 50000, 'Available', 4, 'uploads/image-14-1-2022-941387878.png', '2022-02-14 23:06:05', '2022-02-14 23:42:15');
 
 --
@@ -244,9 +250,9 @@ INSERT INTO `vehicles` (`id`, `category`, `name`, `location`, `description`, `pr
 --
 
 --
--- Indeks untuk tabel `forget_request`
+-- Indeks untuk tabel `forgot_request`
 --
-ALTER TABLE `forget_request`
+ALTER TABLE `forgot_request`
   ADD PRIMARY KEY (`id`),
   ADD KEY `userId` (`userId`),
   ADD KEY `userId_2` (`userId`);
@@ -257,8 +263,7 @@ ALTER TABLE `forget_request`
 ALTER TABLE `histories`
   ADD PRIMARY KEY (`id`),
   ADD KEY `userId` (`userId`,`vehicleId`),
-  ADD KEY `vehicleId` (`vehicleId`),
-  ADD KEY `prepayment` (`prepayment`);
+  ADD KEY `vehicleId` (`vehicleId`);
 
 --
 -- Indeks untuk tabel `prepayment`
@@ -283,16 +288,16 @@ ALTER TABLE `vehicles`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `forget_request`
+-- AUTO_INCREMENT untuk tabel `forgot_request`
 --
-ALTER TABLE `forget_request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+ALTER TABLE `forgot_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT untuk tabel `histories`
 --
 ALTER TABLE `histories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT untuk tabel `prepayment`
@@ -304,7 +309,7 @@ ALTER TABLE `prepayment`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 
 --
 -- AUTO_INCREMENT untuk tabel `vehicles`
@@ -317,18 +322,17 @@ ALTER TABLE `vehicles`
 --
 
 --
--- Ketidakleluasaan untuk tabel `forget_request`
+-- Ketidakleluasaan untuk tabel `forgot_request`
 --
-ALTER TABLE `forget_request`
-  ADD CONSTRAINT `forget_request_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `forgot_request`
+  ADD CONSTRAINT `forgot_request_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `histories`
 --
 ALTER TABLE `histories`
   ADD CONSTRAINT `histories_ibfk_1` FOREIGN KEY (`vehicleId`) REFERENCES `vehicles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `histories_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `histories_ibfk_3` FOREIGN KEY (`prepayment`) REFERENCES `prepayment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `histories_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
