b014dd4aff43411e8f9502872c8d7a43fb1cd3a2
DanangFirmantara <danangfirmantara837@gmail.com>
2022-04-20 11:21:13 +0700

    fix : error issue random destination

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const { responseHandler } = require('./src/helpers/responseHandler');
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(require('./src/routes'));
 app.use('/uploads', express.static('uploads'));
+
+const httpMethods = ['get', 'post', 'put', 'patch', 'delete'];
+
+httpMethods.forEach((el)=>{
+	app[el]('*', (req, res) =>{
+		return responseHandler(res, 404, 'Destination not found');
+	});
+});
 
 const {APP_PORT, PORT} = process.env;
 
--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,15 +1,25 @@
 const mysql = require('mysql');
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 
-
-const db = mysql.createConnection({
+const config = {
 	host : DB_HOST,
 	user : DB_USER,
 	password : DB_PASSWORD,
 	database : DB_NAME
-});
+};
+
+const db = mysql.createPool(config);
+
+console.log('DB Connected');
+
+// const db = mysql.createConnection({
+// 	host : DB_HOST,
+// 	user : DB_USER,
+// 	password : DB_PASSWORD,
+// 	database : DB_NAME
+// });
 
 
-db.connect();
+// db.connect();
 
 module.exports = db;
